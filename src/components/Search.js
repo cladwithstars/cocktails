54516487fdf2c58fffc3ac8bd4bad930846e7cbb
Will Morgan <morgan88will@gmail.com>
2021-11-15 18:14:25 -0500

    change http to https in api calls

--- src/components/Search.js
+++ src/components/Search.js
@@ -41,7 +41,7 @@ export const Search = () => {
   const fetchCocktailBySearchTerm = async () => {
     setLoading(true);
     const { data } = await axios.get(
-      `http://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/search.php?s=${searchTerm}`
+      `https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/search.php?s=${searchTerm}`
     );
     if (data && data.drinks) {
       getCocktails(data);
@@ -53,7 +53,7 @@ export const Search = () => {
   const fetchCocktailByIngredient = async () => {
     setLoading(true);
     const { data } = await axios.get(
-      `http://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/filter.php?i=${searchTerm}`
+      `https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/filter.php?i=${searchTerm}`
     );
     if (data && data.drinks) {
       getCocktails(data);
@@ -64,7 +64,7 @@ export const Search = () => {
 
   const fetchRandomCocktail = async () => {
     const { data } = await axios.get(
-      `http://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/random.php`
+      `https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/random.php`
     );
     if (data) {
       const formattedData = formatData(data);
