3e84a08d1bd1c052b1b9b3d81e3f822d47f898c1
Will Morgan <morgan88will@gmail.com>
2023-02-16 23:40:20 -0500

    modal close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,7 +7,10 @@ export default function Modal({ open }) {
     <>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-pinkGray-200 rounded-t">
